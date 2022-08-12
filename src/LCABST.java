028f178a9f8318494d36f6d4896c733c88948249
gbhat <gbhatweb@gmail.com>
2022-08-12 11:06:28 +0530

    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
    Signed-off-by: gbhat <gbhatweb@gmail.com>

---
+++ src/LCABST.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+public class LCABST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val)
+            return lowestCommonAncestor(root.left, p, q);
+        else if (root.val < p.val && root.val < q.val)
+            return lowestCommonAncestor(root.right, p, q);
+        else
+            return root;
+    }
+
+    class TreeNode {
+        public TreeNode left, right;
+        public int val;
+    }
+}
