e22d4bbdf1ddf325c8624f88782ab7443c7f4c9b
gbhat <gbhatweb@gmail.com>
2022-10-20 12:25:03 +0530

    Update src/TheSkylineProblem.java src/TheSkylineProblem2.java
    Signed-off-by: gbhat <gbhatweb@gmail.com>

--- src/TheSkylineProblem.java
+++ src/TheSkylineProblem.java
@@ -6,21 +6,21 @@ public class TheSkylineProblem {
     public List<List<Integer>> getSkyline(int[][] buildings) {
         List<int[]> heights = new LinkedList<>();
         for(int[] b : buildings) {
-            heights.add(new int[]{b[0], -b[2]});
-            heights.add(new int[]{b[1], b[2]});
+            heights.add(new int[]{b[0], b[2]});
+            heights.add(new int[]{b[1], -b[2]});
         }
 
-        Collections.sort(heights, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        Collections.sort(heights, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
 
         List<List<Integer>> result = new LinkedList<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         pq.offer(0);
         int cur = 0;
         for(int[] h : heights) {
-            if (h[1] < 0)
-                pq.offer(-h[1]);
+            if (h[1] > 0)
+                pq.offer(h[1]);
             else
-                pq.remove(h[1]);
+                pq.remove(-h[1]);
             int top = pq.peek();
             if (cur != top){
                 result.add(List.of(h[0], top));
--- src/TheSkylineProblem2.java
+++ src/TheSkylineProblem2.java
@@ -6,11 +6,11 @@ public class TheSkylineProblem2 {
     public List<List<Integer>> getSkyline(int[][] buildings) {
         List<int[]> heights = new LinkedList<>();
         for(int[] b : buildings) {
-            heights.add(new int[]{b[0], -b[2]});
-            heights.add(new int[]{b[1], b[2]});
+            heights.add(new int[]{b[0], b[2]});
+            heights.add(new int[]{b[1], -b[2]});
         }
 
-        Collections.sort(heights, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        Collections.sort(heights, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
 
         List<List<Integer>> result = new LinkedList<>();
 
@@ -20,14 +20,14 @@ public class TheSkylineProblem2 {
         map.put(0, 1);
         int cur = 0;
         for(int[] h : heights) {
-            if (h[1] < 0)
-                map.put(-h[1], map.getOrDefault(-h[1], 0) + 1);
+            if (h[1] > 0)
+                map.put(h[1], map.getOrDefault(h[1], 0) + 1);
             else {
-                int count = map.get(h[1]) - 1;
+                int count = map.get(-h[1]) - 1;
                 if (count == 0)
-                    map.remove(h[1]);
+                    map.remove(-h[1]);
                 else
-                    map.put(h[1], count);
+                    map.put(-h[1], count);
             }
             int top = map.firstKey();
             if (cur != top){
