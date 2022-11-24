937eca267e0045a20137dcc783e6607f18ec42ef
gbhat <gbhatweb@gmail.com>
2022-11-24 16:12:31 +0530

    Update https://leetcode.com/problems/word-search/
    Signed-off-by: gbhat <gbhatweb@gmail.com>

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -1,33 +1,34 @@
 // https://leetcode.com/problems/word-search/
 
+// Modifies board to track if the character is visited
 public class WordSearch {
+    int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public boolean exist(char[][] board, String word) {
         for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    int[][] lookup = new int[board.length][board[0].length];
-                    if (exist(board, word, lookup, 0, i, j))
-                        return true;
-                }
+            for(int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0) && check(board, word, i, j, 0))
+                    return true;
             }
         }
-
         return false;
     }
 
-    private boolean exist(char[][] board, String word, int[][] lookup, int idx, int i, int j) {
-        if (idx == word.length())
+    private boolean check(char[][] board, String word, int i, int j, int idx) {
+        if (idx == word.length() - 1 && board[i][j] == word.charAt(idx))
             return true;
-
-        if (i < 0 || j < 0 || i >= board.length || j >= board[i].length || word.charAt(idx) != board[i][j] || lookup[i][j] == 1)
+        if (board[i][j] != word.charAt(idx))
             return false;
 
-        lookup[i][j] = 1;
-        if (exist(board, word, lookup, idx + 1, i + 1, j) || exist(board, word, lookup, idx + 1, i - 1, j) ||
-                exist(board, word, lookup, idx + 1, i, j + 1) || exist(board, word, lookup, idx + 1, i, j - 1))
-            return true;
-
-        lookup[i][j] = 0;
+        board[i][j] = '-';
+        for(int[] d : dir) {
+            int next_i = i + d[0];
+            int next_j = j + d[1];
+            if (next_i >= 0 && next_i < board.length && next_j >= 0 && next_j < board[0].length) {
+                if (check(board, word, next_i, next_j, idx + 1))
+                    return true;
+            }
+        }
+        board[i][j] = word.charAt(idx);
         return false;
     }
 }
---
+++ src/WordSearch2.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/word-search/
+
+public class WordSearch2 {
+    public boolean exist(char[][] board, String word) {
+        int[][] lookup = new int[board.length][board[0].length];
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (exist(board, word, lookup, 0, i, j))
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean exist(char[][] board, String word, int[][] lookup, int idx, int i, int j) {
+        if (idx == word.length())
+            return true;
+
+        if (i < 0 || j < 0 || i >= board.length || j >= board[i].length || word.charAt(idx) != board[i][j] || lookup[i][j] == 1)
+            return false;
+
+        lookup[i][j] = 1;
+        if (exist(board, word, lookup, idx + 1, i + 1, j) || exist(board, word, lookup, idx + 1, i - 1, j) ||
+                exist(board, word, lookup, idx + 1, i, j + 1) || exist(board, word, lookup, idx + 1, i, j - 1))
+            return true;
+
+        lookup[i][j] = 0;
+        return false;
+    }
+}
