d3cd03cf27c7bcb2ef68d115d829fb716d5b8007
gbhat <gbhatweb@gmail.com>
2022-11-30 08:14:08 +0530

    Update src/TopKFrequentWords2.java
    Signed-off-by: gbhat <gbhatweb@gmail.com>

--- src/TopKFrequentWords2.java
+++ src/TopKFrequentWords2.java
@@ -3,33 +3,18 @@
 import java.util.*;
 
 public class TopKFrequentWords2 {
-    class Pair implements Comparable<Pair> {
-        public String word;
-        public int freq;
-        public Pair(String word, int freq) {
-            this.word = word;
-            this.freq = freq;
-        }
-
-        public int compareTo(Pair other) {
-            if (this.freq != other.freq)
-                return other.freq - this.freq;
-            return this.word.compareTo(other.word);
-        }
-    }
-
     public List<String> topKFrequent(String[] words, int k) {
-        HashMap<String, Integer> map = new HashMap<>();
-        for(String word : words)
-            map.put(word, map.getOrDefault(word, 0) + 1);
+        Map<String, Integer> map = new HashMap<>();
+        for(String str : words)
+            map.put(str, map.getOrDefault(str, 0) + 1);
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        for(Map.Entry<String, Integer> entry : map.entrySet())
-            pq.add(new Pair(entry.getKey(), entry.getValue()));
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> map.get(b) == map.get(a) ? a.compareTo(b) : map.get(b) - map.get(a));
+        for(String str : map.keySet())
+            pq.offer(str);
 
         List<String> result = new LinkedList<>();
         for(int i = 0; i < k; i++)
-            result.add(pq.poll().word);
+            result.add(pq.poll());
 
         return result;
     }
