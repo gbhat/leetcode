ab9794650bee974c28388765ebc081835e7b1eb3
gbhat <gbhatweb@gmail.com>
2022-12-11 21:35:57 +0530

    renamed: src/LCABST.java->src/BinarySearchTreeLCA.java src/LCABST2.java->src/BinarySearchTreeLCA2.java
    Signed-off-by: gbhat <gbhatweb@gmail.com>

--- src/BinarySearchTreeLCA.java
+++ src/BinarySearchTreeLCA.java
@@ -1,6 +1,6 @@
 //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 
-public class LCABST {
+public class BinarySearchTreeLCA {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root.val > p.val && root.val > q.val)
             return lowestCommonAncestor(root.left, p, q);
--- src/BinarySearchTreeLCA2.java
+++ src/BinarySearchTreeLCA2.java
@@ -4,7 +4,7 @@
     Just walk down from the whole tree's root as long as both p and q are in the same subtree (meaning their values are both smaller or both larger than root's).
     This walks straight from the root to the LCA, not looking at the rest of the tree, so it's pretty much as fast as it gets.
  */
-public class LCABST2 {
+public class BinarySearchTreeLCA2 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         while ((root.val - p.val) * (root.val - q.val) > 0)
             root = p.val < root.val ? root.left : root.right;
